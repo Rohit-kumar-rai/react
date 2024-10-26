31794004d4c8b410423c40e8e34df742614a006b
Rohit-kumar-rai <132555433+Rohit-kumar-rai@users.noreply.github.com>
2024-10-26 17:49:57 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,24 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Weather from './Weather.jsx';
+import{createBrowserRouter,RouterProvider} from 'react-router-dom'
+import { StrictMode } from 'react';
+import Navbar from './Navbar.jsx';
 
-
+const router =createBrowserRouter([
+    {
+    path:"/",
+    element:<App/>},
+    {
+        path:'/Weather',
+        element:<Weather/>
+    }
+]);
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    <StrictMode>
+    
+<RouterProvider router={router}/>
+    </StrictMode>
   
 )
